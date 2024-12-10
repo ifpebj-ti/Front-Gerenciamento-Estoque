c360794a62f5ee53cbcaeaef2d573bc57e30323c
EversonDoNascimento <everson25nascimento@gmail.com>
2024-12-09 23:39:37 -0300

    feat: Created window confirm and loading requests

---
+++ src/app/_components/WindowConfirm/WindowConfirm.tsx
@@ -0,0 +1,44 @@
+"use client";
+type Props = {
+  title: string;
+  message: string;
+  sendClose: () => void;
+  confirm: () => void;
+};
+const WindowConfirm = ({ title, message, sendClose, confirm }: Props) => {
+  return (
+    <div className="bg-black/50 fixed w-full h-full z-40 flex justify-center items-center">
+      <div className="mx-4 sm:mx-0 p-4 sm:p-8 justify-center items-center  bg-white flex flex-col gap-5 sm:gap-8 rounded-lg">
+        <h1 className="font-bold text-lg sm:text-xl">
+          {title ? title : "Tem certeza?"}
+        </h1>
+        <p className="text-center text-sm text-red-700">
+          {message ? message : ""}
+        </p>
+        <div className="flex gap-5 mt-5">
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              sendClose();
+            }}
+            className="uppercase border-2 border-red-500 hover:bg-red-500 text-red-500 hover:text-white w-32 rounded-md py-1"
+          >
+            Cancelar
+          </button>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              confirm();
+              sendClose();
+            }}
+            className="uppercase border-2 border-[var(--color-primary)] hover:bg-[var(--color-primary)] text-[var(--color-primary)] hover:text-white w-32 py-1 rounded-md "
+          >
+            Confirmar
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WindowConfirm;
