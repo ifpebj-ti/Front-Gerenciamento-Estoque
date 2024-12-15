1641f84d1f1921cf37a5ffbb62cd8f6b4ffcb1a8
EversonDoNascimento <everson25nascimento@gmail.com>
2024-12-15 18:48:19 -0300

    feat: install tanstack-query for auxiliary in the requests of API

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import AuthProvider from "./providers/auth-provider";
+import { Providers } from "./../utils/providers";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,7 +31,7 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          {children}
+          <Providers>{children}</Providers>
         </body>
       </html>
     </AuthProvider>
