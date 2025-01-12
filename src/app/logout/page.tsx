ec1e03579112423f292d4e1fe0eb34bfb62c4a59
EversonDoNascimento <everson25nascimento@gmail.com>
2025-01-12 20:13:18 -0300

    fix: error log out 3

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { signOut } from "next-auth/react";
+// import { signOut } from "next-auth/react";
 import { useEffect } from "react";
 import WindowLoad from "../_components/WindowLoad/WindowLoad";
 
