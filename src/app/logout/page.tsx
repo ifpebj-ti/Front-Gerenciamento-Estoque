89308b80c13785d0ee4d949eedb084a945ecc4f2
EversonDoNascimento <everson25nascimento@gmail.com>
2025-01-12 20:19:21 -0300

    fix: error log out 4

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -1,15 +1,16 @@
 "use client";
 
-// import { signOut } from "next-auth/react";
+import { signOut } from "next-auth/react";
 import { useEffect } from "react";
 import WindowLoad from "../_components/WindowLoad/WindowLoad";
 
 const Logout = () => {
   useEffect(() => {
-    // Chame o signOut e defina o callbackUrl para redirecionar após o logout
-    const callbackUrl = `${process.env.NEXT_PUBLIC_URL}/login`; // Use NEXTAUTH_URL do ambiente
-    // signOut({ callbackUrl: callbackUrl });
-    console.log(callbackUrl);
+    (() => {
+      // Chame o signOut e defina o callbackUrl para redirecionar após o logout
+      const callbackUrl = `${process.env.NEXT_PUBLIC_URL}/login`; // Use NEXTAUTH_URL do ambiente
+      return signOut({ callbackUrl: callbackUrl });
+    })();
   }, []);
 
   return <WindowLoad></WindowLoad>;
