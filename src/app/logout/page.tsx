04654e10ed4793ebd15d22094ac5137584d43fc8
EversonDoNascimento <everson25nascimento@gmail.com>
2025-01-12 20:31:51 -0300

    fix: error log out 6

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -7,7 +7,6 @@ import WindowLoad from "../_components/WindowLoad/WindowLoad";
 const Logout = () => {
   useEffect(() => {
     (() => {
-      // Chame o signOut e defina o callbackUrl para redirecionar após o logout
       // const callbackUrl = `${process.env.NEXT_PUBLIC_URL}/login`; // Use NEXTAUTH_URL do ambiente
       return signOut();
     })();
