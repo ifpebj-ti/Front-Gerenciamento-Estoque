9b659c55ce3c90be070d2b85eace91f6d1655cdd
EversonDoNascimento <everson25nascimento@gmail.com>
2025-01-12 20:55:13 -0300

    fix: error log out 8

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -2,17 +2,31 @@
 
 import { signOut } from "next-auth/react";
 import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import WindowLoad from "../_components/WindowLoad/WindowLoad";
 
 const Logout = () => {
+  const router = useRouter();
+
   useEffect(() => {
-    (() => {
-      // const callbackUrl = `${process.env.NEXT_PUBLIC_URL}/login`; // Use NEXTAUTH_URL do ambiente
-      return signOut({
-        callbackUrl: "http://137.131.180.24/login",
-      });
-    })();
-  }, []);
+    const handleLogout = async () => {
+      try {
+        // Chama o signOut para limpar a sessão
+        await signOut({ redirect: false }); // Evita o redirecionamento automático
+
+        // Limpa localStorage e sessionStorage, se necessário
+        localStorage.clear();
+        sessionStorage.clear();
+
+        // Redireciona manualmente para a página desejada
+        router.push("/login");
+      } catch (error) {
+        console.error("Erro ao fazer logout:", error);
+      }
+    };
+
+    handleLogout();
+  }, [router]);
 
   return <WindowLoad></WindowLoad>;
 };
